0289636ddc6b1313a4b2a46dabc532a43b7f4e82
deepak tamta <tamtadeepak31@gmail.com>
2025-12-26 19:28:02 +0530

    Update port for deployment

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(5000, () => {
-    console.log("SERVER RUNNING ON PORT 5000");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
